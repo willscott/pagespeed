44c1c9eb05e853d55045c4658b69f2b93d38a0c8
Will Scott <willscott@gmail.com>
2012-12-27 21:58:47 -0800

    Updated setupjs to work on mac, and note that there isn't an i386 build of phantomjs at the moment.

--- measurement/setup.sh
+++ measurement/setup.sh
@@ -4,17 +4,30 @@
 # Fetch the latest version of phantomjs if not present
 if [ ! -e "phantomjs" ]
 then
+	MAC=`uname`
 	ARCH=`uname -m`
 	DOWNLOAD_PAGE=`curl -s https://code.google.com/p/phantomjs/downloads/list`
-	if [ "$ARCH" == "x86_64" ]
+	if [ "$MAC" == "Darwin" ]
 	then
-		URL=`echo "$DOWNLOAD_PAGE" | sed -n '/phantomjs\.googlecode[^ ]*linux-x86_64.tar.bz2/ s/.*\(phantomjs\.[^ ]*bz2\).*/\1/ p'`
+		URL=`echo "$DOWNLOAD_PAGE" | sed -n '/phantomjs\.googlecode[^ ]*macosx.zip/ s/.*\(phantomjs\.[^ ]*zip\).*/\1/ p'`
 	else
-		URL=`echo "$DOWNLOAD_PAGE" | sed -n '/phantomjs\.googlecode[^ ]*linux-i686.tar.bz2/ s/.*\(phantomjs\.[^ ]*bz2\).*/\1/ p'`	
+		if [ "$ARCH" == "x86_64" ]
+		then
+			URL=`echo "$DOWNLOAD_PAGE" | sed -n '/phantomjs\.googlecode[^ ]*linux-x86_64.tar.bz2/ s/.*\(phantomjs\.[^ ]*bz2\).*/\1/ p'`
+		else
+			echo "There isn't a premade i386 build of phantomjs, you will need to compile it from source."
+			exit 0
+		fi
 	fi
 	curl -s -O "$URL"
-	tar xjf phantomjs*.bz2
-	rm phantomjs*.bz2
+	if [ "$MAC" == "Darwin" ]
+	then
+		unzip phantomjs*.zip
+		rm phantomjs*.zip
+	else
+		tar xjf phantomjs*.bz2
+		rm phantomjs*.bz2
+	fi
 	mv phantomjs*/bin/phantomjs ./
 	rm -r phantomjs-*
 fi
