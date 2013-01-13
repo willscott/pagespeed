6f1e0b856376a2e64ba97ad80badd2ca4b4c7bb5
Will Scott <willscott@gmail.com>
2013-01-12 21:07:06 -0800

    Update to record load time.  Runs against a single URL passed on the command line.

--- measurement/harness.selenium.py
+++ measurement/harness.selenium.py
@@ -1,8 +1,15 @@
 """
 Test harness for page speed measurments using selenium.
 """
-import subprocess
 from selenium import webdriver
+import subprocess
+import sys
+import time
+
+# 0. Get arguments.
+url = sys.argv[1]
+if not url.startswith("http"):
+  url = "http://" + url
 
 # 1. Start up the server.
 server = subprocess.Popen(["java", "-jar", "selenium.jar"])
@@ -15,8 +22,13 @@ profile.set_preference("network.proxy.type", 1)
 
 # 3. Control a client.
 browser = webdriver.Firefox(profile)
-browser.get("http://www.google.com")
+start = time.time()
+browser.get(url)
+end = time.time()
 
-# 4. Clean up.
+# 4. Report.
+print url, (end - start) * 1000
+
+# 5. Clean up.
 browser.close()
 server.kill()
